caf5a3a1dfbe755505c1ee07f5b0e6a7df372492
Marton Kaufmann <kaufmann.marton@protonmail.com>
2023-09-25 01:16:17 +0200

    WIP

--- main.js
+++ main.js
@@ -3,11 +3,42 @@ import Phaser from "phaser"
 class BaseScene extends Phaser.Scene {
 	preload() {
 		this.load.image("play", "public/assets/play.png");
+		this.load.image("play-bg-icon", "public/assets/apple_outline_thic_white.png")
 	}
 
 	create() {
-		const playButtonImage = this.add.image(60, 60, "play");
-		playButtonImage.scale = 0.05;
+		console.log(window.innerWidth)
+		console.log(window.innerHeight)
+		const imgSize = 120*0.8;
+		const imageMargin = 15;
+		let verticalCount = Math.ceil(window.innerHeight / (imgSize+imageMargin)) + 1;
+		let horizontalCount = Math.ceil(window.innerWidth / (imgSize + imageMargin)) + 1;
+
+		console.log(horizontalCount)
+		console.log(verticalCount)
+
+		for (let i = 0; i < verticalCount; i++) {
+			for (let x = 0; x < horizontalCount; x++) {
+				const backgroundImage = this.add.image(
+					(x * imgSize) + (x * imageMargin) + imgSize/2, 
+					(i * imgSize) + (i * imageMargin) + imgSize/2, 
+					"play-bg-icon"
+				)
+				backgroundImage.backgroundImage = "transparent"
+				backgroundImage.scale=0.8
+//				backgroundImage.scale = 0.1
+			}
+		}
+		const playButtonImage = this.add.image(window.innerWidth/2, window.innerHeight/2,"play");
+		playButtonImage.setInteractive();
+		playButtonImage.scale = 0.08;
+		console.log(playButtonImage.width)
+		console.log(playButtonImage.width*0.05)
+		console.log(playButtonImage.height)
+		console.log(playButtonImage.height*0.05)
+		playButtonImage.on("pointerdown", () => {
+			console.log("start playing")
+		})
 	}
 }
 
@@ -16,6 +47,7 @@ const game = new Phaser.Game({
 	width: window.innerWidth,
 	height: window.innerHeight,
 	scene: BaseScene,
+	backgroundColor: "#fff7b4",
 })
 
 console.log("Hello world")
